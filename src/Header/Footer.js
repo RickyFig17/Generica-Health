cbb68653721d6e35f35f5fcb0a06edd984405d6c
ricardo <rfigueroa2317@gmail.com>
2025-02-02 12:58:40 -0500

    adding disclaimer

--- src/Header/Footer.js
+++ src/Header/Footer.js
@@ -17,8 +17,15 @@ function Footer() {
           </li>
         </ul>
       </nav>
+      <Disclaimer />
     </footer>
   );
+}
+
+function Disclaimer() {
+  const disclaim =
+    "This is NOT an official medical website. This is a portfolio project to show a fictional product and should not be used for medical purposes. The creator of Generica Health is not responsible for any medical decisions you choose to make. ALWAYS consult with your healthcare provider for healthcare products that might benefit you and your loved ones.";
+  return <p>{disclaim}</p>;
 }
 
 export default Footer;
