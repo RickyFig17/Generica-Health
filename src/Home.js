6bec2f2d4c133fae4bd68d727836701b8d75c6f8
ricardo <rfigueroa2317@gmail.com>
2025-02-03 01:44:38 -0500

    fixing Medicare div

--- src/Home.js
+++ src/Home.js
@@ -20,9 +20,11 @@ function MedicareHome() {
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
   return (
     <div className="medicare-home-wrapper">
-      <h3>{title}</h3>
-      <p>{message}</p>
-    <img src={oldlady} alt="oldlady" className="oldlady"></img>
+      <div className="title-message-wrapper">
+        <h3>{title}</h3>
+        <p>{message}</p>
+      </div>
+      <img src={oldlady} alt="oldlady" className="oldlady"></img>
     </div>
   );
 }
