dcc93c2fe7fced32b34d2c8f128ece62ddc935e9
ricardo <rfigueroa2317@gmail.com>
2025-02-09 13:44:33 -0500

    adding search bar for ifp

--- src/Ifp.js
+++ src/Ifp.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Ifp() {
-  return <h1>Individual And Family Plan</h1>;
+  return (
+    <>
+      <h1>Individual And Family Plan</h1>
+      <SearchForIfpPlans />
+      <SearchBar/>
+    </>
+  );
+}
+
+function SearchForIfpPlans() {
+  return <h3>Individual and family health insurance plans</h3>;
+}
+
+function SearchBar() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p>Find plans near me: {query}</p>
+    </div>
+  );
 }
 
 export default Ifp;
