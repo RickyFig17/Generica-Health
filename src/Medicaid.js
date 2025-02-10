c76e4734ba67c6323be1b9c64539a5e7a6e0b374
ricardo <rfigueroa2317@gmail.com>
2025-02-10 11:07:50 -0500

    adding searchbar to medicaid

--- src/Medicaid.js
+++ src/Medicaid.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Medicaid() {
-  return <h1>Medicaid</h1>;
+  return (
+    <>
+      <h1>Medicaid</h1>
+      <SearchForMedicaidPlans />
+      <SearchBar />
+    </>
+  );
+}
+
+function SearchForMedicaidPlans() {
+  return <h3>Find plans near me:</h3>;
+}
+
+function SearchBar() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter ZIP Code"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p>{query}</p>
+    </div>
+  );
 }
 
 export default Medicaid;
