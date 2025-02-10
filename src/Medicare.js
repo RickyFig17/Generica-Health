0ff91473fa0faaf5ea06c30e3a2fc8ce60cb30a7
ricardo <rfigueroa2317@gmail.com>
2025-02-10 10:50:15 -0500

    adding medicare searchbar

--- src/Medicare.js
+++ src/Medicare.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Medicare() {
-  return <h1>Medicare</h1>;
+  return (
+    <>
+      <h1>Medicare</h1>
+      <SearchForMedicarePlans />
+      <SearchBar />
+    </>
+  );
+}
+
+function SearchForMedicarePlans() {
+  return <h3>Find plans near me:</h3>;
+}
+
+function SearchBar() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Enter ZIP Code"
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+      />
+      <p>{query}</p>
+    </div>
+  );
 }
 
 export default Medicare;
