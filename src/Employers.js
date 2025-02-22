18de52aaf8b18d9c889606e28316ca4bfdeb1726
ricardo <rfigueroa2317@gmail.com>
2025-02-22 10:34:42 -0500

    adding lorem ipsum

--- src/Employers.js
+++ src/Employers.js
@@ -1,18 +1,25 @@
 import React from "react";
+import "./Employers.scss";
 
 function Employers() {
   return (
     <>
       <h1>Employers</h1>
-      <EmployersHeader/>
+      <EmployersHeader />
     </>
   );
 }
 
 function EmployersHeader() {
   return (
-    <div>
+    <div className="employersHeaderContainer">
       <h3>The Right Plan for Employers</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
     </div>
   );
 }
