d8175f5dbc2a93f0fcf036d913be61ae4db494c8
ricardo <rfigueroa2317@gmail.com>
2025-02-22 10:29:25 -0500

    working on Employers

--- src/Employers.js
+++ src/Employers.js
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Employers() {
-  return <h1>Employers</h1>;
+  return (
+    <>
+      <h1>Employers</h1>
+      <EmployersHeader/>
+    </>
+  );
+}
+
+function EmployersHeader() {
+  return (
+    <div>
+      <h3>The Right Plan for Employers</h3>
+    </div>
+  );
 }
 
 export default Employers;
