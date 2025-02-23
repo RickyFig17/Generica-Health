63de0c3e2edc4d0cfafad2e9df90ecd2bb7adf87
ricardo <rfigueroa2317@gmail.com>
2025-02-23 10:35:54 -0500

    adding providers

--- src/Providers.js
+++ src/Providers.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function Providers() {
-    return<h1>Providers</h1>
+  return (
+    <>
+      <h1>Providers</h1>
+      <ProvidersHeader />
+    </>
+  );
+}
+
+function ProvidersHeader() {
+  return (
+    <div>
+      <h3>Healthcare for Providers</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat.
+      </p>
+    </div>
+  );
 }
 
 export default Providers;
