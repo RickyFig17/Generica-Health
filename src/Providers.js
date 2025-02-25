cf9fa3edbc56d122b416807399578ed00f260572
ricardo <rfigueroa2317@gmail.com>
2025-02-25 16:34:27 -0500

    adding more stuff

--- src/Providers.js
+++ src/Providers.js
@@ -17,7 +17,7 @@ function ProvidersHeader() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        commodo consequat.And other things.
       </p>
     </div>
   );
