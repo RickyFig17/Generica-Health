002d42c79215bf371cccc4d40fd005b5832f7bb6
ricardo <rfigueroa2317@gmail.com>
2025-02-24 20:51:22 -0500

    adding brokers

--- src/Brokers.js
+++ src/Brokers.js
@@ -2,7 +2,24 @@ import React from "react";
 import "./Brokers.scss";
 
 function Brokers() {
-  return <h1>Brokers</h1>;
+  return (
+    <>
+      <h1>Brokers</h1>
+      <BrokersHeader />
+    </>
+  );
+}
+
+function BrokersHeader() {
+  <div>
+    <h3>healthcare for Brokers</h3>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </p>
+  </div>;
 }
 
 export default Brokers;
