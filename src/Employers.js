7f9c96a4c95e9a2b91338e06dec15c3482c66fa9
ricardo <rfigueroa2317@gmail.com>
2025-02-28 16:20:25 -0500

    adding more on employers

--- src/Employers.js
+++ src/Employers.js
@@ -18,7 +18,7 @@ function EmployersHeader() {
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
+        commodo consequat.And more things too.
       </p>
     </div>
   );
