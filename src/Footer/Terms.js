6574031107684b8d3c7ee88f3b5bd5e25039b718
ricardo <rfigueroa2317@gmail.com>
2025-03-01 18:01:31 -0500

    adding text to terms

--- src/Footer/Terms.js
+++ src/Footer/Terms.js
@@ -1,7 +1,42 @@
 import React from "react";
+import TextContent from "../Text.json";
 
 function Terms() {
-  return <h3>Terms Of Service</h3>;
+  return (
+    <>
+      <h3>{TextContent.TextContent.Terms.termsOfService}</h3>
+      <h5>
+        {TextContent.TextContent.Terms.effectiveDate}
+        {TextContent.TextContent.Terms.januaryFirst}
+      </h5>
+      <p>
+        <h5>{TextContent.TextContent.Terms.welcome}</h5>
+        <ul>
+          <li>{TextContent.TextContent.Terms.websiteUsage}</li>
+          <li>{TextContent.TextContent.Terms.onlyInfoContent}</li>
+          <li>{TextContent.TextContent.Terms.accuracyNotGuaranteed}</li>
+          <li>{TextContent.TextContent.Terms.noUlawfulThings}</li>
+          <li>{TextContent.TextContent.Terms.noMedAdvice}</li>
+          <li>{TextContent.TextContent.Terms.generalInfoOnly}</li>
+          <li>{TextContent.TextContent.Terms.healthProviderAdvice}</li>
+          <li>{TextContent.TextContent.Terms.intellectualProperty}</li>
+          <li>{TextContent.TextContent.Terms.publicStockImages}</li>
+          <li>{TextContent.TextContent.Terms.propertyRightInfringement}</li>
+          <li>{TextContent.TextContent.Terms.limitiationOfLiability}</li>
+          <li>{TextContent.TextContent.Terms.notRespForUseOfWebsite}</li>
+          <li>{TextContent.TextContent.Terms.noInterruptionsGuaranteed}</li>
+          <li>{TextContent.TextContent.Terms.thirdPartyLinks}</li>
+          <li>{TextContent.TextContent.Terms.mayContain3rdParty}</li>
+          <li>{TextContent.TextContent.Terms.noRespEndorsement}</li>
+          <li>{TextContent.TextContent.Terms.changeOfTerms}</li>
+          <li>{TextContent.TextContent.Terms.mayUpdateTerms}</li>
+          <li>{TextContent.TextContent.Terms.updateTermAcc}</li>
+          <li>{TextContent.TextContent.Terms.governingLaw}</li>
+          <li>{TextContent.TextContent.Terms.interpretedTerms}</li>
+        </ul>
+      </p>
+    </>
+  );
 }
 
 export default Terms;
