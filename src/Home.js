93d2c8c542fc32c826e27ec3adf611f4a4247279
ricardo <rfigueroa2317@gmail.com>
2025-03-01 19:01:36 -0500

    adding link to medicare home

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import "./Home.scss";
 import oldlady from "./images/oldlady.png";
 import family from "./images/family.png";
 import ifp from "./images/ifp.png";
+import { Link } from "react-router-dom";
 
 function Home() {
   return (
@@ -19,11 +20,13 @@ function Home() {
 function MedicareHome() {
   const title = "Medicare";
   const message =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    "Quality care, affordable coverage, Medicare has you covered.";
   return (
     <div className="medicare-home-wrapper">
       <div className="title-message-wrapper">
-        <h3>{title}</h3>
+        <Link to="/medicare">
+          <h3>{title}</h3>
+        </Link>
         <p>{message}</p>
       </div>
       <img src={oldlady} alt="oldlady" className="oldlady"></img>
@@ -34,7 +37,7 @@ function MedicareHome() {
 function MedicaidHome() {
   const title = "Medicaid";
   const message =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    "Affordable care and stronger communities, Medicaid makes it possible.";
   return (
     <div className="medicaid-home-wrapper">
       <div className="title-message-wrapper">
@@ -48,8 +51,7 @@ function MedicaidHome() {
 
 function IfpHome() {
   const title = "Individual and Family Plan";
-  const message =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  const message = "Flexible health coverage, built for you and your family.";
   return (
     <div className="ifp-home-wrapper">
       <div className="title-message-wrapper">
