c097ae87e1964c7f38e576c46c8877c07ccad13c
ricardo <rfigueroa2317@gmail.com>
2025-03-03 09:15:13 -0500

    working on employers

--- src/Employers.js
+++ src/Employers.js
@@ -15,10 +15,12 @@ function EmployersHeader() {
     <div className="employersHeaderContainer">
       <h3>The Right Plan for Employers</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.And more things too.
+        Protect your business and your team with comprehensive health insurance
+        designed for employers. Our flexible plans offer quality coverage,
+        competitive rates, and essential benefits to keep your employees healthy
+        and productive. Invest in their well-being while enhancing job
+        satisfaction and retention. Get a quote today and build a stronger
+        workplace with the right coverage!
       </p>
     </div>
   );
