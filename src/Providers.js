488426cb17f0b6363b576bce7496f45134347516
ricardo <rfigueroa2317@gmail.com>
2025-03-07 18:01:47 -0500

    moving providers to Text.json

--- src/Providers.js
+++ src/Providers.js
@@ -1,9 +1,10 @@
 import React from "react";
+import TextContent from "./Text.json";
 
 function Providers() {
   return (
     <>
-      <h1>Providers</h1>
+      <h1>{TextContent.TextContent.Providers.providers}</h1>
       <ProvidersHeader />
     </>
   );
@@ -12,14 +13,8 @@ function Providers() {
 function ProvidersHeader() {
   return (
     <div>
-      <h3>Healthcare for Providers</h3>
-      <p>
-        Empower your practice with comprehensive healthcare solutions designed
-        for providers. Our tailored plans offer reliable coverage, competitive
-        rates, and essential benefits to support you in delivering quality care.
-        Focus on your patients while we take care of your insurance needs. Get
-        started today and secure the coverage you deserve!
-      </p>
+      <h3>{TextContent.TextContent.Providers.healthcareProviders}</h3>
+      <p>{TextContent.TextContent.Providers.empowerHealthSolutions}</p>
     </div>
   );
 }
