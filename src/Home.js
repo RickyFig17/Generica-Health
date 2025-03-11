4e6c1a8731a39d3ec68ccd78b7faf2e1290b29dc
ricardo <rfigueroa2317@gmail.com>
2025-03-11 09:50:30 -0400

    adding home to text.json

--- src/Home.js
+++ src/Home.js
@@ -4,11 +4,14 @@ import oldlady from "./images/oldlady.png";
 import family from "./images/family.png";
 import ifp from "./images/ifp.png";
 import { Link } from "react-router-dom";
+import TextContent from "./Text.json";
 
 function Home() {
   return (
     <>
-      <h1 className="home-header">Generica Health</h1>
+      <h1 className="home-header">
+        {TextContent.TextContent.Home.genericaHealth}
+      </h1>
       <h2>{<MedicareHome />}</h2>
       <h2>{<MedicaidHome />}</h2>
       <h2>{<IfpHome />}</h2>
@@ -18,16 +21,13 @@ function Home() {
 }
 
 function MedicareHome() {
-  const title = "Medicare";
-  const message =
-    "Quality care, affordable coverage, Medicare has you covered.";
   return (
     <div className="medicare-home-wrapper">
       <div className="title-message-wrapper">
         <Link to="/medicare">
-          <h3>{title}</h3>
+          <h3>{TextContent.TextContent.Home.medicare}</h3>
         </Link>
-        <p>{message}</p>
+        <p>{TextContent.TextContent.Home.medicareMessage}</p>
       </div>
       <img src={oldlady} alt="oldlady" className="oldlady"></img>
     </div>
@@ -35,16 +35,13 @@ function MedicareHome() {
 }
 
 function MedicaidHome() {
-  const title = "Medicaid";
-  const message =
-    "Affordable care and stronger communities, Medicaid makes it possible.";
   return (
     <div className="medicaid-home-wrapper">
       <div className="title-message-wrapper">
         <Link to="/medicaid">
-          <h3>{title}</h3>
+          <h3>{TextContent.TextContent.Home.medicaid}</h3>
         </Link>
-        <p>{message}</p>
+        <p>{TextContent.TextContent.Home.medicaidMessage}</p>
       </div>
       <img src={family} alt="family" className="family"></img>
     </div>
@@ -52,15 +49,13 @@ function MedicaidHome() {
 }
 
 function IfpHome() {
-  const title = "Individual and Family Plan";
-  const message = "Flexible health coverage, built for you and your family.";
   return (
     <div className="ifp-home-wrapper">
       <div className="title-message-wrapper">
         <Link to="/ifp">
-          <h3>{title}</h3>
+          <h3>{TextContent.TextContent.Home.ifp}</h3>
         </Link>
-        <p>{message}</p>
+        <p>{TextContent.TextContent.Home.ifpMessage}</p>
       </div>
       <img src={ifp} alt="ifp" className="ifp"></img>
     </div>
@@ -70,19 +65,19 @@ function IfpHome() {
 function OtherInfo() {
   return (
     <div className="otherinfo-wrapper">
-      <h1>Other Services</h1>
+      <h1>{TextContent.TextContent.Home.otherServices}</h1>
       <div className="health-wrapper">
         <div className="behavioral">
-          <h3>Behavioral Health</h3>
+          <h3>{TextContent.TextContent.Home.behavioralHealth}</h3>
         </div>
         <div className="dental">
-          <h3>Dental Health</h3>
+          <h3>{TextContent.TextContent.Home.dentalHealth}</h3>
         </div>
         <div className="vision">
-          <h3>Vision Health</h3>
+          <h3>{TextContent.TextContent.Home.visionHealth}</h3>
         </div>
         <div className="physical">
-          <h3>Physical Health</h3>
+          <h3>{TextContent.TextContent.Home.physicalHealth}</h3>
         </div>
       </div>
     </div>
